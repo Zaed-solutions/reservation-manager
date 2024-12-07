82fcd34aa739c3cbec04387ae19b106d24507315
Mohamed_Dev <mohamedzarea29@gmail.com>
2024-12-07 13:04:33 +0200

    added : nav drawer

--- app/src/main/java/com/zaed/reservationmanager/ui/MainActivity.kt
+++ app/src/main/java/com/zaed/reservationmanager/ui/MainActivity.kt
@@ -7,9 +7,21 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material3.DrawerValue
+import androidx.compose.material3.ExtendedFloatingActionButton
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Icon
+import androidx.compose.material3.ModalDrawerSheet
+import androidx.compose.material3.ModalNavigationDrawer
+import androidx.compose.material3.NavigationDrawerItem
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.navigation.compose.rememberNavController
 import com.zaed.reservationmanager.app.navigation.NavigationHost
 import com.zaed.reservationmanager.ui.theme.ReservationManagerTheme
@@ -29,14 +41,27 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun App() {
     val navController = rememberNavController()
-    Scaffold(
-        modifier = Modifier
-            .fillMaxSize()
-            .imePadding(),
-    ) { paddingValues ->
+    val drawerState = rememberDrawerState(initialValue = DrawerValue.Closed)
+    val scope = rememberCoroutineScope()
+    ModalNavigationDrawer(
+        drawerState = drawerState,
+        drawerContent = {
+            ModalDrawerSheet {
+                Text("Drawer title", modifier = Modifier.padding(16.dp))
+                HorizontalDivider()
+                NavigationDrawerItem(
+                    label = { Text(text = "Drawer Item") },
+                    selected = false,
+                    onClick = { /*TODO*/ }
+                )
+            }
+        },
+    ) {
         NavigationHost(
-            modifier = Modifier.padding(paddingValues),
+            modifier = Modifier,
             navController = navController,
         )
     }
+
+
 }
