99c79175a864149cfce13697c5e0ff83d0a4665c
Mohamed_Dev <mohamedzarea29@gmail.com>
2024-12-12 11:17:04 +0200

    feat/#16/implement create reservation screen:fix minor issue

--- app/src/main/java/com/zaed/reservationmanager/ui/reservation/create/ReservationUiAction.kt
+++ app/src/main/java/com/zaed/reservationmanager/ui/reservation/create/ReservationUiAction.kt
@@ -11,7 +11,6 @@ sealed class ReservationUiAction {
     data object Cancel : ReservationUiAction()
     data class UpdateReservationDate(val date: Long?) : ReservationUiAction()
     data class UpdateReservationTime(val time: Long?) : ReservationUiAction()
-    data object AddReservation : ReservationUiAction()
     data class UpdateDriver(val driver: Employee) : ReservationUiAction()
     data class UpdateReservationType(val type: String) : ReservationUiAction()
     data class UpdateReservationCar(val car: String) : ReservationUiAction()
